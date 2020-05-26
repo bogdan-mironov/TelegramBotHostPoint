afc464f6908ff533b0487d25d86a4f699545d2a7
Bogdan Mironov <mad.rod123@outlook.com>
2020-05-26 18:51:10 +0300

    бум

--- TelegramBotHostPoint/Events.cs
+++ TelegramBotHostPoint/Events.cs
@@ -1,10 +1,84 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using static TelegramBotHostPoint.Program;
+using Telegram.Bot.Types.ReplyMarkups;
 
 namespace TelegramBotHostPoint
 {
     class Events
     {
+        public async static void ShowKeyboard()
+        {
+            var inlineKeyboard = new InlineKeyboardMarkup(new[]
+{               // first row
+                new []
+                {
+                    InlineKeyboardButton.WithCallbackData("Yes", "Yes"),
+                    InlineKeyboardButton.WithCallbackData("No", "No")
+                }});
+            await Bot.SendTextMessageAsync(chat_id, "Wanna search for proxy servers with filters?", replyMarkup: inlineKeyboard);
+        }
+        public async static void Bot_OnCallbackQuery(object sc, CallbackQueryEventArgs ev)
+        {
+            var message = ev.CallbackQuery.Message;
+            switch (ev.CallbackQuery.Data)
+            {
+                // Поиск по фильтру
+                case ("Yes"):
+                    await Bot.SendTextMessageAsync(chat_id,
+                        "out - тип файла, в котором будет результат поиска" +
+                        "\nmaxtime - максимальная задержка" +
+                        "\nports - порты" +
+                        "\ntype - протоколы" +
+                        "\nuptime - аптайм" +
+                        "\ncountry - страны");
+                    break;
+                case ("No"):
+                    Fetch();
+                    break;
+            }
+           }
+        public async static void Bot_OnMessage(object sender, Telegram.Bot.Args.MessageEventArgs e)
+        {
+            chat_id = e.Message.Chat.Id;
+            if (e.Message.Text == "Search")
+            {
+                ShowKeyboard();
+            }
+            else if (e.Message.Text.Contains('='))
+            {
+
+                string text = e.Message.Text;
+                int length = text.Length;
+                string _out = text.Substring(length - 3).Contains('=') ? text.Substring(length - 2) : text.Substring(length - 3);
+                string datastring = null;
+                /*
+                try
+                {
+                    string filter = e.Message.Text.Split(' ')[1];
+                    string address = "http://apigetfile.azurewebsites.net/HostPoint/GetFile?filter=" + filter;
+                    datastring = client.DownloadString(address).Replace('\"', ' ');
+
+                    byte[] rawdata = Convert.FromBase64String(datastring);
+                    System.IO.File.WriteAllBytes($"log.{_out}", rawdata);
+                    using (var stream = System.IO.File.Open($"log.{_out}", FileMode.OpenOrCreate))
+                    {
+                        InputOnlineFile iof = new InputOnlineFile(stream);
+                        iof.FileName = $"Chery Blossom.{_out}";
+                        await Bot.SendDocumentAsync(chat_id, iof, "");
+                    }
+
+                }
+                catch (Exception)
+                {
+
+                    Bot.SendTextMessageAsync(chat_id, $"Error: incorrect filters");
+                }
+                */
+            }
+        }
     }
 }
--- TelegramBotHostPoint/Program.cs
+++ TelegramBotHostPoint/Program.cs
@@ -12,130 +12,38 @@ namespace TelegramBotHostPoint
     class Program
     {
         static readonly WebClient client = new WebClient();
-        private static  TelegramBotClient Bot = new TelegramBotClient("1095007957:AAFkRRUrMpWNxciWiTidT91bebZs9NQG3hQ");
-        static long  chat_id;
-        static Dictionary<string, string> filters = new Dictionary<string, string>() {
-                    {"maxtime","1000"}, {"country",""},
-                    { "ports", "" }, {"type",""},
-                    {"anon",""}, {"uptime",""}, {"out","&out=plain" }
-                };
-        static string _out = "plain";
+        public static TelegramBotClient Bot = new TelegramBotClient("1095007957:AAFkRRUrMpWNxciWiTidT91bebZs9NQG3hQ");
+        public static long chat_id {get;set;}
+        static string _out = "txt";
         static void Main(string[] args)
         {
-            Bot.OnMessage += Bot_OnMessage;
-            Bot.OnCallbackQuery += async (object sc, Telegram.Bot.Args.CallbackQueryEventArgs ev) =>
-            {
-                var message = ev.CallbackQuery.Message;
-                if (ev.CallbackQuery.Data == "11")
-                {
-                    Bot.SendTextMessageAsync(chat_id,"Available filters:\nmaxtime - servers with latency above this variable won't appear" +
-                        "\ncountry = include servers hosted in them. Type their 2symbol code (UA for example) " +
-                        "\nports = desirable ports (80,3000-4000,4001 for example)" +
-                        "\ntype = type of proxy(for HTTP type h, HTTPS - s,SOCKS4- 4,SOCKS5 - 5. )" +
-                        "\nanon=anonimity level (1-none,2-low,3-medium,4-high)" +
-                        "\nout= desirable format (csv, xml, js, php are available)" +
-                        "\nExample how to search with filters:\n/start &maxtime=600&country=UARU&type=s45&out=csv" +
-                        "\n Parameter out  is neccessary and has to be entered in the end when searching by filter.");
-
-                }
-                else
-                if (ev.CallbackQuery.Data == "22")
-                {
-                    Fetch();
-                }
-            };
+            Bot.OnMessage += Events.Bot_OnMessage;
+            Bot.OnCallbackQuery += Events.Bot_OnCallbackQuery;
             
             Bot.StartReceiving();
             Console.ReadLine();
             Bot.StopReceiving();
         }
-
-        private async static void Bot_OnMessage(object sender, Telegram.Bot.Args.MessageEventArgs e)
+        static string address = "http://apigetfile.azurewebsites.net/HostPoint/GetFile?filter=";
+        public async static void Fetch(string filter = "maxtime=800;out=plain")
         {
-           chat_id = e.Message.Chat.Id;
-            if (e.Message.Text == "/start")
+            try
             {
-                var inlineKeyboard = new InlineKeyboardMarkup(new[]
-{               // first row
-                new []
+                string datastring = client.DownloadString(address + filter).Trim('\"');
+                byte[] rawdata = Convert.FromBase64String(datastring);
+                File.WriteAllBytes($"output.{_out}", rawdata);
+                using (FileStream stream = File.Open($"output.{_out}", FileMode.Open))
                 {
-                    InlineKeyboardButton.WithCallbackData("Yes", "11"),
-                    InlineKeyboardButton.WithCallbackData("No", "22")
-                }});
-                Bot.SendTextMessageAsync(chat_id, "Wanna search for proxy servers with filters?", replyMarkup: inlineKeyboard);
-
+                    InputOnlineFile file = new InputOnlineFile(stream);
+                    file.FileName = $"out.{_out}";
+                    await Bot.SendDocumentAsync(chat_id, file, "");
+                }
             }
-            else if (e.Message.Text.Contains('='))
+            catch (Exception)
             {
-
-                string text = e.Message.Text;
-                int length = text.Length;
-                string _out = text.Substring(length - 3).Contains('=') ? text.Substring(length - 2) : text.Substring(length - 3);
-                string datastring = null;
-                try
-                {
-
-                    string address = "http://apigetfile.azurewebsites.net/HostPoint/GetFile?filter=" + e.Message.Text.Split(' ')[1].Replace('\"', ' ');
-                     Console.WriteLine(address);
-                     datastring = client.DownloadString(address).Replace('\"',' ');
-                    
-                    byte[] rawdata = Convert.FromBase64String(datastring);
-                    System.IO.File.WriteAllBytes($"log.{_out}", rawdata);
-                    using (var stream = System.IO.File.Open($"log.{_out}", FileMode.OpenOrCreate))
-                    {
-                        InputOnlineFile iof = new InputOnlineFile(stream);
-                        iof.FileName = $"Chery Blossom.{_out}";
-                        await Bot.SendDocumentAsync(chat_id, iof, "");
-                    }
-
-                }
-                catch (Exception)
-                {
-
-                    Bot.SendTextMessageAsync(chat_id,$"Error: incorrect filters");
-                    Console.WriteLine(datastring);
-                    throw;
-                }
-                finally
-                {
-
-
-                }
+                Bot.SendTextMessageAsync(chat_id, "Ошибочка выполнения");
             }
-        }
-        /*private  static void FetchByFilter()
-        {
-            var inlineKeyboard = new InlineKeyboardMarkup(new[]
-{               // first row
-                new []
-                {
-                    InlineKeyboardButton.WithCallbackData("Max latency", "11"),
-                    InlineKeyboardButton.WithCallbackData("Ports", "12")
-                },
-                // second row
-                new []
-                {
-                    InlineKeyboardButton.WithCallbackData("Uptime", "21"),
-                    InlineKeyboardButton.WithCallbackData("Types", "22"),
-                },
-                new[]
-                {
-                    InlineKeyboardButton.WithCallbackData("Anonimity","31"),
-                    InlineKeyboardButton.WithCallbackData("Countries","32")
-                }
-            });
-            Bot.SendTextMessageAsync(chat_id, "Filters?", replyMarkup: inlineKeyboard);
-            }
-            */
-        private async static void Fetch()
-        {
-
-            byte[] data = client.DownloadData("http://apigetfile.azurewebsites.net/HostPoint/GetFile");
-            System.IO.File.WriteAllBytes($"log.txt", data);
-            var stream = System.IO.File.Open($"log.txt", FileMode.OpenOrCreate);
-            InputOnlineFile iof = new InputOnlineFile(stream);
-            iof.FileName = $"Chery Blossom.txt";
-            await Bot.SendDocumentAsync(chat_id, iof, "");
+            
         }
     }
 }
