7c3b434bad61b83864990efd1892fa1470362824
Bogdan Mironov <mad.rod123@outlook.com>
2020-05-27 01:58:43 +0300

    обновка

--- TelegramBotHostPoint/Events.cs
+++ TelegramBotHostPoint/Events.cs
@@ -5,6 +5,8 @@ using Telegram.Bot;
 using Telegram.Bot.Args;
 using static TelegramBotHostPoint.Program;
 using Telegram.Bot.Types.ReplyMarkups;
+using Telegram.Bot.Types.InputFiles;
+using System.IO;
 
 namespace TelegramBotHostPoint
 {
@@ -16,68 +18,46 @@ namespace TelegramBotHostPoint
 {               // first row
                 new []
                 {
-                    InlineKeyboardButton.WithCallbackData("Yes", "Yes"),
-                    InlineKeyboardButton.WithCallbackData("No", "No")
+                    InlineKeyboardButton.WithCallbackData("Да", "Yes"),
+                    InlineKeyboardButton.WithCallbackData("Нет", "No")
                 }});
-            await Bot.SendTextMessageAsync(chat_id, "Wanna search for proxy servers with filters?", replyMarkup: inlineKeyboard);
+            await Bot.SendTextMessageAsync(chat_id, "Ищем прокси по фильтру?", replyMarkup: inlineKeyboard);
         }
         public async static void Bot_OnCallbackQuery(object sc, CallbackQueryEventArgs ev)
         {
-            var message = ev.CallbackQuery.Message;
+            
             switch (ev.CallbackQuery.Data)
             {
                 // Поиск по фильтру
                 case ("Yes"):
                     await Bot.SendTextMessageAsync(chat_id,
-                        "out - тип файла, в котором будет результат поиска" +
-                        "\nmaxtime - максимальная задержка" +
-                        "\nports - порты" +
-                        "\ntype - протоколы" +
-                        "\nuptime - аптайм" +
-                        "\ncountry - страны");
+                        "out - тип файла, в котором будет выдан результат поиска" +
+                        "\n\nmaxtime - максимальная задержка" +
+                        "\n\nports - порты \nВводите порты через запятую или тире (8080,2000-3000)" +
+                        "\n\ntype - протоколы \nВводите в параметр код протокола (Для HTTP - h, HTTPS - s,SOCKS4 - 4,SOCKS5 -5, можно вводить несколько)" +
+                        "\n\ncountry - прокси, запущенные из этих стран, будут выведены в результате поиска (Используйте 2-х значный код страны по ISO 3166-1)" +
+                        "\n\nПример поиска по фильтру: /start maxtime=1000;type=4,s,h;out=csv" +
+                        "\n\nПараметр out обязательно должен быть в конце");
+
                     break;
                 case ("No"):
-                    Fetch();
+                    Fetch(_out:"plain");
                     break;
             }
            }
         public async static void Bot_OnMessage(object sender, Telegram.Bot.Args.MessageEventArgs e)
         {
             chat_id = e.Message.Chat.Id;
-            if (e.Message.Text == "Search")
+            if (e.Message.Text == "/start")
             {
                 ShowKeyboard();
             }
-            else if (e.Message.Text.Contains('='))
+            else if (e.Message.Text.StartsWith("/start") && e.Message.Text.Contains("out="))
             {
-
                 string text = e.Message.Text;
-                int length = text.Length;
-                string _out = text.Substring(length - 3).Contains('=') ? text.Substring(length - 2) : text.Substring(length - 3);
-                string datastring = null;
-                /*
-                try
-                {
-                    string filter = e.Message.Text.Split(' ')[1];
-                    string address = "http://apigetfile.azurewebsites.net/HostPoint/GetFile?filter=" + filter;
-                    datastring = client.DownloadString(address).Replace('\"', ' ');
-
-                    byte[] rawdata = Convert.FromBase64String(datastring);
-                    System.IO.File.WriteAllBytes($"log.{_out}", rawdata);
-                    using (var stream = System.IO.File.Open($"log.{_out}", FileMode.OpenOrCreate))
-                    {
-                        InputOnlineFile iof = new InputOnlineFile(stream);
-                        iof.FileName = $"Chery Blossom.{_out}";
-                        await Bot.SendDocumentAsync(chat_id, iof, "");
-                    }
-
-                }
-                catch (Exception)
-                {
-
-                    Bot.SendTextMessageAsync(chat_id, $"Error: incorrect filters");
-                }
-                */
+                string _out = text.Substring(text.Length - 3);
+                string filter = e.Message.Text.Split(' ')[1];
+                Fetch(_out,filter);
             }
         }
     }
--- TelegramBotHostPoint/Program.cs
+++ TelegramBotHostPoint/Program.cs
@@ -11,10 +11,11 @@ namespace TelegramBotHostPoint
 {
     class Program
     {
-        static readonly WebClient client = new WebClient();
+        public static readonly WebClient client = new WebClient();
         public static TelegramBotClient Bot = new TelegramBotClient("1095007957:AAFkRRUrMpWNxciWiTidT91bebZs9NQG3hQ");
         public static long chat_id {get;set;}
         static string _out = "txt";
+        static List<string> availableformats = new List<string>() { "txt", "json", "xml", "csv", "php" };
         static void Main(string[] args)
         {
             Bot.OnMessage += Events.Bot_OnMessage;
@@ -25,10 +26,12 @@ namespace TelegramBotHostPoint
             Bot.StopReceiving();
         }
         static string address = "http://apigetfile.azurewebsites.net/HostPoint/GetFile?filter=";
-        public async static void Fetch(string filter = "maxtime=800;out=plain")
+        public async static void Fetch(string _out, string filter = "maxtime=800;out=plain")
         {
             try
             {
+                _out = GetFormat(_out);
+                Console.WriteLine(address + filter);
                 string datastring = client.DownloadString(address + filter).Trim('\"');
                 byte[] rawdata = Convert.FromBase64String(datastring);
                 File.WriteAllBytes($"output.{_out}", rawdata);
@@ -41,9 +44,20 @@ namespace TelegramBotHostPoint
             }
             catch (Exception)
             {
-                Bot.SendTextMessageAsync(chat_id, "Ошибочка выполнения");
+                Bot.SendTextMessageAsync(chat_id, "Ошибка выполнения");
             }
             
+        }
+        public static  string GetFormat(string _out)
+        {
+            if (_out == "plain") return "txt";
+            if(_out == "js") return "json";
+            if (!availableformats.Contains(_out))
+            {
+                Bot.SendTextMessageAsync(chat_id, "Вы ввели неверный фильтр");
+                throw  new ArgumentException();
+            }
+            return _out;
         }
     }
 }
